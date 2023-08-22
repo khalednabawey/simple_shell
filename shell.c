bc8092f60a3f8abb889e7f51a053538811c6ae7a
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-08-22 16:56:41 -0700

    main code_3

--- shell.c
+++ shell.c
@@ -19,3 +19,19 @@ int parse_input(char *input, char **args)
 	}
 	return (index);
 }
+/**
+ * search_command - Searches and executes the specified command
+ * @args: Array of arguments
+ *
+ * Return: None
+ */
+
+void search_command(char **args)
+{
+	char *command, *path, *path_copy, *directory, *full_command;
+
+	command = strdup(args[0]);
+	if (command == NULL)
+		exit(1);
+
+}
