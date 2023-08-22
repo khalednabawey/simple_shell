c877e664017e7caf8068d71606223bd993fe8e00
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-08-22 16:48:47 -0700

    main code

---
+++ shell.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * parse_input - Parses user input into individual commands and arguments
+ * @input: User input to be parsed
+ * @args: Array of arguments
+ *
+ * Return: Number of commands and arguments
+ */
+
