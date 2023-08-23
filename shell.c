a5ccfae0ef2e62c7fdbd7b3bbc8d2c1d9e6ee378
khalednabawey <5alednabawi@gmail.com>
2023-08-23 01:09:56 -0700

    tp main

--- shell.c
+++ shell.c
@@ -84,3 +84,47 @@ void print_environment(void)
 		index++;
 	}
 }
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char input[150];
+	char *args[20];
+	pid_t pid;
+	int count;
+
+	while (1)
+	{
+		if (fgets(input, 150, stdin) == NULL)
+			exit(0);
+
+		input[strcspn(input, "\n")] = 0;
+
+		if (strcmp(input, "exit") == 0)
+			break;
+		else if (strcmp(input, "env") == 0)
+		{
+			print_environment();
+			continue;
+		}
+
+		pid = fork();
+		if (pid == 0)
+		{
+			count = parse_input(input, args);
+			args[count] = NULL;
+
+			search_command(args);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	return (0);
+}
+
